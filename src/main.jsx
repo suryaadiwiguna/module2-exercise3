96f1db9f7df97ec1c75e290b351c8943d0afdb82
suryaadiwiguna <suryaadiwiguna@gmail.com>
2023-11-18 15:59:37 +0700

    3

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './App.css'
-import Nav from './components/nav.jsx'
+import NavBar from './components/navbar.jsx'
 import Heading from './components/Heading.jsx'
 import Profile from './components/Profile.jsx'
 import Idea from './components/Idea.jsx'
@@ -10,7 +10,7 @@ import Capabilities from './components/Capabilities.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Nav />
+    <NavBar />
     <Heading />
     <Profile />
     <Idea />
