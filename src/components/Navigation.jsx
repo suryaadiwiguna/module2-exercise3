27ba3880766e49fedfaabe19cb9230189eddd08a
suryaadiwiguna <suryaadiwiguna@gmail.com>
2023-11-20 13:47:05 +0700

    added Navigation.jsx

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,27 @@
+import website_logo from '../assets/logo.svg'
+
+function Navigation() {
+    return (
+        <div id='sticky'>
+            <div >
+                <div className='navbar'>
+                    <div className='navbar-left'>
+                        <a href='/'>
+                            <img src={website_logo} className='website-logo' />
+                        </a>
+
+                    </div >
+
+                    <div className='hamburger'>
+                        <div className='burger burger1' />
+                        <div className='burger burger2' />
+                        <div className='burger burger3' />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './App.css'
-import NavBar from './components/navbar.jsx'
+import Navigation from './components/Navigation.jsx'
 import Heading from './components/Heading.jsx'
 import Profile from './components/Profile.jsx'
 import Idea from './components/Idea.jsx'
@@ -10,7 +10,7 @@ import Capabilities from './components/Capabilities.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NavBar />
+    <Navigation />
     <Heading />
     <Profile />
     <Idea />
